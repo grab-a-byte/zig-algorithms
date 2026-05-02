f21a380176e91c0c4c72f24dd87eca3716a22c60
Adam Parker <30392071+grab-a-byte@users.noreply.github.com>
2026-05-02 09:02:30 +0100

    Fix merge sort

--- src/main.zig
+++ src/main.zig
@@ -7,26 +7,26 @@ pub fn main(init: std.process.Init) !void {
     const allocator = init.arena.allocator();
 
     var vec: std.ArrayList(u32) = .empty;
-    for (0..4) |_| {
+    for (0..10_000) |_| {
         try vec.append(allocator, rand.int(u32) % 10_000);
     }
 
     //Insertion sort timing
     const in_sort_values = try allocator.dupe(u32, vec.items);
-    print_array(in_sort_values, "before: ");
+    // print_array(in_sort_values, "before: ");
     var start = std.Io.Timestamp.now(init.io, .real).toMilliseconds();
     zig_sorting.insertion_sort(in_sort_values);
     var end = std.Io.Timestamp.now(init.io, .real).toMilliseconds();
-    print_array(in_sort_values, "");
+    // print_array(in_sort_values, "");
     std.debug.print("{d}\n", .{end - start});
 
     //Merge sort timing
     const merge_sort_values = try allocator.dupe(u32, vec.items);
-    print_array(merge_sort_values, "before: ");
+    // print_array(merge_sort_values, "before: ");
     start = std.Io.Timestamp.now(init.io, .real).toMilliseconds();
     try zig_sorting.merge_sort(allocator, merge_sort_values);
     end = std.Io.Timestamp.now(init.io, .real).toMilliseconds();
-    print_array(merge_sort_values, "");
+    // print_array(merge_sort_values, "");
     std.debug.print("{d}\n", .{end - start});
 }
 
--- src/root.zig
+++ src/root.zig
@@ -13,44 +13,34 @@ pub fn insertion_sort(items: []u32) void {
 
 //Merge Sort
 pub fn merge_sort(allocator: std.mem.Allocator, items: []u32) !void {
-    try merge_sort_parameterized(allocator, items, 0, items.len);
+    try merge_sort_parameterized(allocator, items);
 }
 
-fn merge_sort_parameterized(allocator: std.mem.Allocator, items: []u32, left: usize, right: usize) !void {
-    std.debug.print("left: {d} && right {d}\n", .{ left, right });
-    if (left >= right) return;
+fn merge_sort_parameterized(allocator: std.mem.Allocator, items: []u32) !void {
+    if (items.len == 1) return;
+    const middle = items.len / 2;
+    try merge_sort_parameterized(allocator, items[0..middle]);
+    try merge_sort_parameterized(allocator, items[middle..]);
 
-    const middle = left + (right - left) / 2;
-    std.debug.print("middle: {d}\n", .{middle});
-    try merge_sort_parameterized(allocator, items, left, middle);
-    try merge_sort_parameterized(allocator, items, middle + 1, right);
-
-    try merge(allocator, items, left, middle, right);
+    try merge(allocator, items);
 }
 
-fn merge(allocator: std.mem.Allocator, output: []u32, left: usize, middle: usize, right: usize) !void {
-    std.debug.print("merge {d} {d} {d}\n", .{ left, middle, right });
-    const left_array = try allocator.dupe(u32, output[left..middle]);
+fn merge(allocator: std.mem.Allocator, output: []u32) !void {
+    const middle = output.len / 2;
+    const left_array = try allocator.dupe(u32, output[0..middle]);
     defer allocator.free(left_array);
-    const right_array = try allocator.dupe(u32, output[middle..right]);
+    const right_array = try allocator.dupe(u32, output[middle..]);
     defer allocator.free(right_array);
-    print_array(left_array, "left");
-    print_array(right_array, "right");
 
     var left_count: u32 = 0;
     var right_count: u32 = 0;
 
-    var index = left;
+    var index: usize = 0;
     while (left_count < left_array.len and right_count < right_array.len) {
-        // std.debug.print("hit\n", .{});
-        // std.debug.print("{d}\n", .{left_array[left_count]});
-        // std.debug.print("{d}\n", .{right_array[right_count]});
         if (left_array[left_count] < right_array[right_count]) {
-            // std.debug.print("hit2\n", .{});
             output[index] = left_array[left_count];
             left_count += 1;
         } else {
-            // std.debug.print("hit3\n", .{});
             output[index] = right_array[right_count];
             right_count += 1;
         }
@@ -69,32 +59,19 @@ fn merge(allocator: std.mem.Allocator, output: []u32, left: usize, middle: usize
         right_count += 1;
         index += 1;
     }
-
-    print_array(output, "");
 }
 
-fn print_array(items: []u32, prefix: []const u8) void {
-    std.debug.print("{s}", .{prefix});
-    std.debug.print("[", .{});
-    for (items) |i| {
-        std.debug.print("{d},", .{i});
-    }
-    std.debug.print("]\n", .{});
+test "merge sort 2 items" {
+    var input = [_]u32{ 32, 12 };
+    try merge_sort(std.testing.allocator, input[0..]);
+
+    try std.testing.expect(input[0] == 12);
+    try std.testing.expect(input[1] == 32);
 }
-
-// test "merge sort 2 items" {
-//     var input = [_]u32{ 32, 12 };
-//     try merge_sort(std.testing.allocator, input[0..]);
-
-//     try std.testing.expect(input[0] == 12);
-//     try std.testing.expect(input[1] == 32);
-// }
 
 test "merge sort with 4 items" {
     var input = [_]u32{ 32, 12, 24, 1 };
     try merge_sort(std.testing.allocator, input[0..]);
-
-    print_array(&input, "");
 
     try std.testing.expect(input[0] == 1);
     try std.testing.expect(input[1] == 12);
