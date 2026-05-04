72cc264c221ac244fc8c60530ac99dd4555551a5
Adam Parker <30392071+grab-a-byte@users.noreply.github.com>
2026-05-04 21:43:41 +0100

    update merge sort and swapping syntax and also start of trying to implement quicksort

--- src/sorting.zig
+++ src/sorting.zig
@@ -13,14 +13,10 @@ pub fn insertion_sort(items: []u32) void {
 
 //Merge Sort
 pub fn merge_sort(allocator: std.mem.Allocator, items: []u32) !void {
-    try merge_sort_parameterized(allocator, items);
-}
-
-fn merge_sort_parameterized(allocator: std.mem.Allocator, items: []u32) !void {
     if (items.len == 1) return;
     const middle = items.len / 2;
-    try merge_sort_parameterized(allocator, items[0..middle]);
-    try merge_sort_parameterized(allocator, items[middle..]);
+    try merge_sort(allocator, items[0..middle]);
+    try merge_sort(allocator, items[middle..]);
 
     try merge(allocator, items);
 }
@@ -61,14 +57,13 @@ fn merge(allocator: std.mem.Allocator, output: []u32) !void {
     }
 }
 
+//Heap sort
 pub fn heap_sort(items: []u32) void {
     const n = items.len - 1;
     build_max_heap(items);
     for (0..n) |index| {
         const i = n - index;
-        const first = items[0];
-        items[0] = items[i];
-        items[i] = first;
+        items[0], items[i] = .{ items[i], items[0] };
         max_heapify(items, i, 0);
     }
 }
@@ -89,12 +84,33 @@ fn max_heapify(items: []u32, heap_size: usize, index: usize) void {
     largest = if (right < heap_size and items[right] > items[largest]) right else largest;
 
     if (largest != index) {
-        const value = items[largest];
-        items[largest] = items[index];
-        items[index] = value;
+        items[largest], items[index] = .{ items[index], items[largest] };
         max_heapify(items, heap_size, largest);
     }
 }
+
+//Quicksort
+// pub fn quicksort(items: []u32, start: usize, end: usize) void {
+//     if (start > end) return;
+//     std.debug.print("going from {d} to {d}\n", .{ start, end });
+//     const middle = partition(items, start, end);
+//     quicksort(items, start, middle);
+//     quicksort(items, middle, end);
+// }
+
+// fn partition(items: []u32, low: usize, high: usize) usize {
+//     const pivot = items[high];
+//     var i: u32 = low - 1;
+//     for (low..high - 1) |index| {
+//         if (items[index] <= pivot) {
+//             i = i + 1;
+//             std.mem.swap(u32, &items[i], &items[index]);
+//         }
+//     }
+
+//     std.mem.swap(u32, &items[i + 1], &items[high]);
+//     return i + 1;
+// }
 
 test "merge sort 2 items" {
     var input = [_]u32{ 32, 12 };
@@ -113,3 +129,16 @@ test "merge sort with 4 items" {
     try std.testing.expect(input[2] == 24);
     try std.testing.expect(input[3] == 32);
 }
+
+// test "Quicksort should sort" {
+//     var input = [_]u32{ 2, 8, 7, 1, 3, 5, 6, 4 };
+//     quicksort(&input, 0, input.len - 1);
+//     try std.testing.expect(input[0] == 1);
+//     try std.testing.expect(input[1] == 2);
+//     try std.testing.expect(input[2] == 3);
+//     try std.testing.expect(input[3] == 4);
+//     try std.testing.expect(input[4] == 5);
+//     try std.testing.expect(input[5] == 6);
+//     try std.testing.expect(input[6] == 7);
+//     try std.testing.expect(input[7] == 8);
+// }
